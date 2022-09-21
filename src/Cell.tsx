306c007807fce76ae1b73648aeee21ab7cc0b9a7
Tarmanau <itarmanau@ibagroup.eu>
2022-09-21 13:48:59 +0200

    Добавил стратегии для фигур. Добавил утилитарные функции.
    Обновил нейминг переменных и изображений

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,54 +1,56 @@
 import * as React from 'react'
 import {FC} from 'react'
-import {CellI, CellType, FigureI} from "./types";
+import {CellI, ColorType, FigureI} from "./types";
 import styles from './Cell.module.sass'
-import {Horse, horseStrategy} from "./utils";
+import {isXYEqual} from "./data/utils";
 
 
 interface Props{
     cell: CellI,
-    figures: FigureI[],
-    setFigures: (arr: FigureI[]) => void,
     selectedFigure: FigureI | null,
     setSelectedFigure: (value: FigureI | null) => void,
-    setKilled: (figure: FigureI) => void
+    isAvailable: boolean,
+    figures: FigureI[]
+    setFigures: (data: FigureI[]) => void
 }
 
 
-export const Cell: FC<Props> = ({cell,figures, setFigures, selectedFigure, setSelectedFigure, setKilled }) => {
-    const click = (target: CellI) => {
-        if(target.figure?.color !== selectedFigure?.color){
-            const result = figures.map(i => {
-                if(i.position.x === selectedFigure?.position.x && i.position.y === selectedFigure?.position.y){
-                    i.position = target.position
-                    i.steps.push(target.position)
-                    return i
-                }
-                else{
-                    return i
-                }
-            } )
-            setFigures(result);
-            if(selectedFigure){
-                horseStrategy(selectedFigure, target)
+export const Cell: FC<Props> = (
+    {
+        cell,
+        selectedFigure,
+        setSelectedFigure,
+        isAvailable,
+        figures,
+        setFigures }
+) => {
+    const onMove = () => {
+        if(selectedFigure){
+            if(isAvailable){
+                const result = figures.map(i => {
+                    if(isXYEqual(i.position, selectedFigure?.position)){
+                        i.position = cell.position
+                        i.steps.push(cell.position)
+                        return i
+                    }
+                    else{
+                        return i
+                    }
+                })
+                setFigures(result);
             }
-            if(target.figure){
-                setKilled(target.figure)
-            }//костыль
         }
-        setSelectedFigure(null);
     }
     return (
         <div className={` ${ styles.cell}
-         ${cell.type === CellType.BLACK ? styles.cell_black : styles.cell_white}
-         ${cell.position.x === selectedFigure?.position.x && cell.position.y === selectedFigure?.position.y && styles.selected}
+         ${cell.type === ColorType.BLACK ? styles.cell_black : styles.cell_white}
+         ${selectedFigure && isXYEqual(cell.position, selectedFigure?.position) && styles.selected}
          `}
-             onClick={()=> {
-                 if(selectedFigure){
-                     click(cell)
-                 }
-             }}
-            >
+            onClick={onMove}
+        >
+            {selectedFigure && selectedFigure?.strategy(selectedFigure, cell ) &&
+                <div className={styles.available}>*</div> }
+
             x: {cell.position.x}
             y: {cell.position.y}
             {cell.figure && <img onClick={()=> setSelectedFigure(cell.figure)} src={cell.figure.img}/>}
---
+++ src/data/models.ts
@@ -0,0 +1,87 @@
+import {ColorType, FigureI, FigureType, Position} from "../types";
+import white_knight_img from "../assets/knight_white.png";
+import black_knight_img from "../assets/knight_black.png";
+import black_bishop_img from "../assets/bishop_black.png";
+import white_bishop_img from "../assets/bishop_white.png";
+import black_queen_img from "../assets/queen_black.png";
+import white_queen_img from "../assets/queen_white.png";
+import black_king_img from "../assets/king_black.png";
+import white_king_img from "../assets/king_white.png";
+import black_pawn_img from "../assets/black_pawn.png";
+import white_pawn_img from "../assets/white_pawn.png";
+import black_rook_img from "../assets/rook_black.png";
+import white_rook_img from "../assets/rook_white.png";
+import {bishopStrategy, horseStrategy, kingStrategy, pawnStrategy, queenStrategy, rookStrategy} from "./strategies";
+import {isBlack} from "./utils";
+
+export const Knight = (color: ColorType, id: string, position: Position,): FigureI => {
+    return {
+        color: isBlack(color) ? ColorType.BLACK : ColorType.WHITE,
+        img: isBlack(color) ? black_knight_img : white_knight_img ,
+        strategy: horseStrategy,
+        type: FigureType.KNIGHT,
+        position: position,
+        id: id,
+        steps: []
+    }
+}
+
+export const Bishop = (color: ColorType, id: string, position: Position,): FigureI => {
+    return {
+        color: isBlack(color) ? ColorType.BLACK : ColorType.WHITE,
+        img: isBlack(color) ? black_bishop_img : white_bishop_img ,
+        strategy: bishopStrategy,
+        type: FigureType.BISHOP,
+        steps: [],
+        position: position,
+        id: id
+    }
+}
+
+export const Rook = (color: ColorType, id: string, position: Position,): FigureI => {
+    return {
+        color: isBlack(color) ? ColorType.BLACK : ColorType.WHITE,
+        img: isBlack(color) ? black_rook_img : white_rook_img ,
+        strategy: rookStrategy,
+        type: FigureType.ROOK,
+        steps: [],
+        position: position,
+        id: id
+    }
+}
+
+export const Pawn = (color: ColorType, id: string, position: Position,): FigureI => {
+    return {
+        color: isBlack(color) ? ColorType.BLACK : ColorType.WHITE,
+        img: isBlack(color) ? black_pawn_img : white_pawn_img ,
+        strategy: pawnStrategy,
+        type: FigureType.PAWN,
+        steps: [],
+        position: position,
+        id: id
+    }
+}
+
+export const King = (color: ColorType, id: string, position: Position,): FigureI => {
+    return {
+        color: isBlack(color) ? ColorType.BLACK : ColorType.WHITE,
+        img: isBlack(color) ? black_king_img : white_king_img ,
+        strategy: kingStrategy,
+        type: FigureType.KING,
+        steps: [],
+        position: position,
+        id: id
+    }
+}
+
+export const Queen = (color: ColorType, id: string, position: Position,): FigureI => {
+    return {
+        color: isBlack(color) ? ColorType.BLACK : ColorType.WHITE,
+        img: isBlack(color) ? black_queen_img : white_queen_img ,
+        strategy: queenStrategy,
+        type: FigureType.QUEEN,
+        steps: [],
+        position: position,
+        id: id
+    }
+}
